3d70b2a21fdfad58f4e15e48f3cd2a48127856e2
“NguyenThanhLu” <ludc24v7k017@vlvh.ctu.edu.vn>
2026-05-02 19:15:42 +0700

    first commit be2

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req,resp) => {
+    resp.send({message: "create handle"})
+}
+
+exports.findAll = (req,resp) => {
+    resp.send({message: "findAll handle"})
+}
+
+exports.findOne = (req,resp) => {
+    resp.send({message: "findOne handle"})
+}
+
+exports.update = (req,resp) => {
+    resp.send({message: "update handle"})
+}
+
+exports.delete = (req,resp) => {
+    resp.send({message: "delete handle"})
+}
+
+exports.deleteAll = (req,resp) => {
+    resp.send({message: "deleteAll handle"})
+}
+
+exports.findAllFavorite = (req,resp) => {
+    resp.send({message: "findAllFavorite handle"})
+}
